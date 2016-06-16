5213edd83bcff5c6b993eb4f18b9909e1c17cf17
Coco <308695699@qq.com>
2016-06-16 21:22:21 +0800

    update

--- img2div/js/index.js
+++ img2div/js/index.js
@@ -1,8 +1,8 @@
 ;/*!/static/js/index.js*/
-(function(window,undefined){
+(function(window, undefined) {
 
 	var
-		img2div = function(imgElem){
+		img2div = function(imgElem) {
 			return new img2div.prototype.init(imgElem);
 		},
 		img = document.getElementById('img'),
@@ -14,7 +14,7 @@
 	 * 监听图片上传事件
 	 * @return {[type]} [description]
 	 */
-	function imgInputListener(){
+	function imgInputListener() {
 		var input = document.getElementById("fileId1");
 
 		if (typeof FileReader === 'undefined') {
@@ -29,19 +29,19 @@
 	 * 上传图片处理
 	 * @return {[type]} [description]
 	 */
-	function readFile(){
-    var file = this.files[0];
-    if(!/image\/\w+/.test(file.type)){
-        alert("文件必须为图片！");
-        return false;
-    }
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function(e){
-      result.innerHTML = '<img id="img" src="'+this.result+'" alt=""/>'
-  		img = document.getElementById('img');
-  		img2div(img);
-    }
+	function readFile() {
+		var file = this.files[0];
+		if (!/image\/\w+/.test(file.type)) {
+			alert("文件必须为图片！");
+			return false;
+		}
+		var reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = function(e) {
+			result.innerHTML = '<img id="img" src="' + this.result + '" alt=""/>'
+			img = document.getElementById('img');
+			img2div(img);
+		}
 	}
 
 	/**
@@ -49,60 +49,60 @@
 	 * @param  {[type]} imgElem [description]
 	 * @return {[type]}         [description]
 	 */
-  img2div.prototype.getImgColor = function(imgElem){
-  	var imgWidth = imgElem.width,
+	img2div.prototype.getImgColor = function(imgElem) {
+		var imgWidth = imgElem.width,
 			imgHeight = imgElem.height,
 			canvas = document.createElement("canvas"),
 			context = canvas.getContext("2d");
 
 		// 根据 img 设置 canvas 高宽
 		canvas.width = imgWidth;
-  	canvas.height = imgHeight;
+		canvas.height = imgHeight;
 
-  	// 绘图
-  	context.drawImage(imgElem, 0, 0, imgWidth, imgHeight);
+		// 绘图
+		context.drawImage(imgElem, 0, 0, imgWidth, imgHeight);
 
-  	// 取色
-  	var colorArr = context.getImageData(0, 0, imgWidth, imgHeight),
-  	  data = colorArr.data,
-  	  len = data.length,
-  	  boxShadowBox = [],
-  	  i = 0;
+		// 取色
+		var colorArr = context.getImageData(0, 0, imgWidth, imgHeight),
+			data = colorArr.data,
+			len = data.length,
+			boxShadowBox = [],
+			i = 0;
 
-  	for(; i<len; i++){
-  		if(i%4 === 0){
-  			var x = i / 4 % imgWidth,
-  				y = Math.floor(i /4 / imgWidth),
-  				rgbaR = data[i],
-  				rgbaG = data[i+1],
-  				rgbaB = data[i+2],
-  				rgbaA = data[i+3];
+		for (; i < len; i++) {
+			if (i % 4 === 0) {
+				var x = i / 4 % imgWidth,
+					y = Math.floor(i / 4 / imgWidth),
+					rgbaR = data[i],
+					rgbaG = data[i + 1],
+					rgbaB = data[i + 2],
+					rgbaA = data[i + 3];
 
-  			boxShadowBox.push(
-  				(x+1)+'px '+
-  				(y+1)+'px '+ 'rgba(' +
-  				rgbaR + ',' + rgbaG + ',' + rgbaB + ',' + rgbaA + ')'
-  			);
-  		}
-  	}
-  	// console.log(data);
-  	// console.log(boxShadowBox);
-  	// console.log('len is '+len);
+				boxShadowBox.push(
+					(x + 1) + 'px ' +
+					(y + 1) + 'px ' + 'rgba(' +
+					rgbaR + ',' + rgbaG + ',' + rgbaB + ',' + rgbaA + ')'
+				);
+			}
+		}
+		// console.log(data);
+		// console.log(boxShadowBox);
+		// console.log('len is '+len);
 
-  	// div.style.width = imgWidth;
-  	// div.style.height = imgHeight;
-  	div.style.marginBottom = imgHeight+20+'px';
-  	div.style.boxShadow = boxShadowBox.join();
-  	code.innerText = '<div style="width:1px;height:1px;box-shadow:'+ boxShadowBox.join() + '">';
+		// div.style.width = imgWidth;
+		// div.style.height = imgHeight;
+		div.style.marginBottom = imgHeight + 20 + 'px';
+		div.style.boxShadow = boxShadowBox.join();
+		code.innerText = '<div style="width:1px;height:1px;box-shadow:' + boxShadowBox.join() + '">';
 
-  }
+	}
 
-  img2div.prototype.init = function(imgElem){
-  	this.getImgColor(imgElem);
-  }
+	img2div.prototype.init = function(imgElem) {
+		this.getImgColor(imgElem);
+	}
 
-  img2div.prototype.init.prototype = img2div.prototype;
+	img2div.prototype.init.prototype = img2div.prototype;
 
 
-  imgInputListener();
+	imgInputListener();
 })(window);
